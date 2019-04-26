e76337ca3bd8924195494a36f092c9f51b32dc61
sara goncalves <goncalvesarag@gmail.com>
2019-04-26 17:05:36 -0300

    Bricando com estruturaDeRepetição

---
+++ conhecendoORuby.rb
@@ -0,0 +1,8 @@
+puts 'Digite seu nome'
+nome = gets.chomp
+puts 'Digite sua idade'
+idade = gets.chomp
+puts 'Onde você mora'
+cidade = gets.chomp
+
+puts "Eu sou #{nome}, tenho #{idade} anos e sou de #{cidade}"
---
+++ estruturaDeRepeticao.rb
@@ -0,0 +1,27 @@
+#------------------#
+#      	MENU       #
+#------------------#
+puts '[1] Inserir nova tarefa'
+puts '[2] Ver todas as tarefas'
+puts '[3] Sair'
+
+opcao = gets.to_i
+tarefa = [""]
+op = 10
+
+
+while op <= 10
+   if opcao == 1 
+	puts 'Digite nova tarefa'
+	tarefa = gets.chomp()
+	op += 1
+	puts 'tarefa cadastrada: ' + tarefa
+	
+ 
+   elsif opcao == 2 
+	puts tarefa
+
+   else
+	puts 'Opção Inválida'
+   end
+end
